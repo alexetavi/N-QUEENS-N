4cddd9375bc6a7c0021d74f93d29c04812bfca15
alexetavi <39312294+alexetavi@users.noreply.github.com>
2018-09-10 18:49:34 +0300

    submit

---
+++ #10AlexeOctavianAlexandru/#10AlexeOctavianAlexandru/Docs/html/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['place',['place',['../functions_8h.html#ac5e1175302b4733dc9e3b919d33140ad',1,'place.c']]],
+  ['print',['print',['../functions_8h.html#a99ed1c00a6bcb150111479e48d730f18',1,'print.c']]]
+];
---
+++ #10AlexeOctavianAlexandru/#10AlexeOctavianAlexandru/Docs/html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['queen',['queen',['../functions_8h.html#ade01689ac636b28e9821af1971abf3f2',1,'queen.c']]]
+];
